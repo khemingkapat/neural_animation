90e2070cc4e2a066e40acf35262d8eeb91b107c7
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-12-13 14:12:37 +0700

    add streamlit package

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
                 python311Packages.numpy
                 python311Packages.matplotlib
                 python311Packages.pandas
+                python311Packages.streamlit
 
                 ];
             };
