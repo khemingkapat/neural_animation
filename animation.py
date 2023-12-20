b348ed9881612ca9f82220d212dace0ac4f51b2e
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-12-20 14:41:44 +0700

    testing colormap function

--- animation.py
+++ animation.py
@@ -1,27 +1,7 @@
-from matplotlib import animation
 import numpy as np
-import matplotlib
+from mycolorpy import colorlist as mcp
 
-from matplotlib import cm
-import matplotlib.pyplot as plt
-from matplotlib.animation import PillowWriter
-import seaborn as sns
-
-fig = plt.figure()
-data_list = []
-for _ in range(100):
-    data_list.append(np.random.randn(10, 10))
-
-
-def init():
-    sns.heatmap(np.zeros((10, 10)), vmax=0.8, cbar=False)
-
-
-def animate(i):
-    data = data_list[i]
-    sns.heatmap(data, vmax=0.8, cbar=False)
-
-
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=20)
-writer = animation.PillowWriter(fps=20)
-anim.save("heatmap.gif", writer=writer)
+data = np.random.randn(10)
+print(data.shape)
+color1 = mcp.gen_color_normalized(cmap="inferno", data_arr=data)
+print(color1)
